3a8bf29bd09593aeb353510e78444e66dd30fcc6
Connor Cleary <77419962+connorcleary@users.noreply.github.com>
2022-11-18 13:49:30 +1300

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
 import coastal_aquifer_model as cam
 from pars import ModelParameters, load_parameters
-import results
+#  import results
 
 def create_run_plot_model(name, **kwargs):
     """
@@ -17,15 +17,15 @@ def create_run_plot_model(name, **kwargs):
     swt = cam.build_steady_model(pars)
     cam.run_model(swt)
     concentration, head, qx, qy, qz = cam.extract_results(name)
-    results.plot_results(name)
-    results.plot_evolutions(name)
-    results.save_metrics(name, fraction=0.05)
-    results.plot_boundary_concentration(name)
+    # results.plot_results(name)
+    # results.plot_evolutions(name)
+    # results.save_metrics(name, fraction=0.05)
+    # results.plot_boundary_concentration(name)
 
 
 def main():
     #create_run_plot_model("elongated", Lx=2000)
-    create_run_plot_model("wetland9d_low", h_b = -1.1545, x_w=50, Lx_w=10, z_w=-0.75, h_w=-0.21, dt=50)
+    create_run_plot_model("wetland9d_low", h_b = -1.1545, x_w=50, Lx_w=10, z_w=-0.75, h_w=-0.21)
 
 if __name__=="__main__":
     main()
